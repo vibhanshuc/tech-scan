6828e3600676aa0840cbc09c998254a009dd2889
Vibhanshu <vibhanshu86@gmail.com>
2017-09-03 03:56:28 +0530

    Update ReadME

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
 const TopNav = () => (
-  <AppBar showMenuIconButton={false} title={<Link to={'/'}>TechScan</Link>}/>
+  <AppBar showMenuIconButton={false}
+          title={<Link to={'/'}>TechScan</Link>}
+          iconElementRight={<FlatButton href={'http://vibhanshu.me'} target="_blank" label="Portfolio"/>}
+  />
 );
 
 export default TopNav;
